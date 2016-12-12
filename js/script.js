a6fd6f30127ccd4135f3fe21bd2e57aec8d32aed
1CTWgha <whitakertchristopher@gmail.com>
2016-12-12 14:50:48 -0800

    pick a random card

--- js/script.js
+++ js/script.js
@@ -10,15 +10,16 @@ initalizeDeck();
 function initalizeDeck(){
   for (var i = 0; i<cardNames.length; i++){//loops through cardNames
     var card = cardNames[i];//saves selected card
-    var cardValues = getCardValue(cardNames[i]);
     deck.push(card);//push cards
-    deck.push(cardValues);
     // console.log(card);//prints all 52 cards
+
+    var cardValues = getCardValue(cardNames[i]);//saves cardNames value
+    deck.push(cardValues);//push value
   }
 }
 
-function getCardValue(cardName){
-  var rank = cardName.split("_", 1)[0];//divide argument by "_" only once similar to parsInt; {gives me a string not an array}
+function getCardValue(cardName){//still returns a "string"!!!!!!!???????
+  var rank = cardName.split("_", 1)[0];//divide argument by "_" only once similar to parsInt; {gives me a string not an array not sure if [] still needed at the end}
   //want strings so that I can evaluate face cards and aces
   var valueOne = parseInt(rank, 10);
   if (isNaN(valueOne)){ //if not a number
@@ -31,13 +32,37 @@ function getCardValue(cardName){
     else {// only remaing possibilities are numeric cards
       return [valueOne];//so return the numeric card value
     }
-  }
+}
 
 function getRandomNumber (min, max){//order maders
-  return Math.floor(Math.random()*(max - min + 1));//
-}
-
-function pickACard(){//use above to get a card
-
+  return Math.floor(Math.random()*(max - min + 1));//gives me a random number when values entered
 }
 // Min + (int)(Math.random() * ((Max - Min) + 1))
+
+function pickACard(){//use above to get a card
+  do {
+    var c = getRandomNumber(0, 51);//picks random number in the deck
+    if (!deck[c].dealt){//deal the card if it has not been dealt
+      deck[c].dealt = true;// if not dealt then set dealt to true(removes card from deck(dealable cards))
+      return deck[c];//need to track this better?
+      }
+    }
+    while (true);//when true keep dealing
+    //if not true meaning deck[c] has been dealt false (if card is drawn false)
+  }
+
+function evaluateHand(){
+
+}
+
+function runPlayer(){//look at and evaluate players hand
+
+ }
+
+function runDealer(){//look at and evaluate dealer hand
+
+ }
+
+ function displayScore(){
+
+ }
